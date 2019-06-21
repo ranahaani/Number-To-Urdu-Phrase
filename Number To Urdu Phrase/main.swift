48360c09b0bd6265ab86ef0770ba6ce5b7cdb577
Muhammad Abdullah <muhammadabdullah@Muhammads-MacBook-Pro.local>
2019-06-21 09:44:47 -0700

    Initial Commit

---
+++ Number To Urdu Phrase/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Number To Urdu Phrase
+//
+//  Created by Muhammad Abdullah on 21/06/2019.
+//  Copyright © 2019 Muhammad Abdullah. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
